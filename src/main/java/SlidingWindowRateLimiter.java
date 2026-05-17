17e3a869cf0764177977b3d3162968d7c1badf33
derivstat99 <hqfftapotheosis644@gmail.com>
2026-05-18 00:05:18 +0530

    sliding window algo with timestamp

--- src/main/java/SlidingWindowRateLimiter.java
+++ src/main/java/SlidingWindowRateLimiter.java
@@ -1,5 +1,39 @@
 package main.java;
 
-public class SlidingWindowRateLimiter {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.LinkedList;
 
+public class SlidingWindowRateLimiter implements RateLimiter {
+    private final int limit;
+    private final long windowSizeInSeconds;
+    private final Map<String, LinkedList<Long>> requestLogs;
+
+    public SlidingWindowRateLimiter(int limit, long windowSizeInSeconds) {
+        this.limit = limit;
+        this.windowSizeInSeconds = windowSizeInSeconds;
+        requestLogs = new HashMap<>();
+    }
+
+    @Override
+    public synchronized boolean allowRequest(String userId) {
+        long now = System.currentTimeMillis();
+
+        if (!requestLogs.containsKey(userId)) {
+            requestLogs.put(userId, new LinkedList<>());
+        }
+
+        LinkedList<Long> userLog = requestLogs.get(userId);
+
+        while (!userLog.isEmpty() && (now - userLog.getFirst()) / 1000.0 > windowSizeInSeconds) {
+            userLog.removeFirst();
+        }
+
+        if (userLog.size() < limit) {
+            userLog.addLast(now);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
